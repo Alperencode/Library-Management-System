e3cf0f3340f168b1bb71082bde6085cb9adf19f5
Alperen Ağa <alperencode@gmail.com>
2025-02-24 09:52:26 +0300

    feat: add expection handlers

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from internal.api.login import router as login_router
+from internal.api.exception_handlers import generic_exception_handler, validation_exception_handler
 
 app = FastAPI()
 
+# Routers
 app.include_router(login_router, prefix="/api/v1", tags=["Authenticate and Session Management"])
+
+# Exception handlers
+app.add_exception_handler(Exception, generic_exception_handler)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
