539947b0ca6000860bc107ebd56f65732572bbc1
Alperen Ağa <alperencode@gmail.com>
2025-02-24 15:52:38 +0300

    fix: config usage

--- internal/tokens/tokens.py
+++ internal/tokens/tokens.py
@@ -1,14 +1,12 @@
 import jwt
 from datetime import datetime, timedelta
-from config.config import (
-    SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_DAYS
-)
+from config.config import get_config
 
 
 def create_jwt_token(data, expires_delta: timedelta):
     to_encode = data.copy()
     to_encode["exp"] = datetime.now() + expires_delta
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, get_config("secret_key"), algorithm=get_config("algorithm"))
 
 
 def create_access_token(user):
@@ -19,19 +17,19 @@ def create_access_token(user):
             "email": user["email"],
             "role": user["role"]
         },
-        timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        timedelta(minutes=get_config("access_token_expire_minutes"))
     )
 
 
 def create_refresh_token(user):
     return create_jwt_token(
         {"user_id": user["user_id"]},
-        timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+        timedelta(days=get_config("refresh_token_expire_days"))
     )
 
 
 def verify_jwt_token(token):
     try:
-        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return jwt.decode(token, get_config("secret_key"), algorithms=[get_config("algorithm")])
     except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
         return None
