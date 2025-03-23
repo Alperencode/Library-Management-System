386bc5eae7b7c5cc222542f8ab4f6479111824f7
Alperen Ağa <alperencode@gmail.com>
2025-03-23 21:18:28 +0300

    feat: Add book operations (#14)
    * Add fuzzy search support
    * Add book model, database operations and endpoints
    * Fix startup database check bugs

--- internal/database/database.py
+++ internal/database/database.py
@@ -1,9 +1,5 @@
-import sys
-import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
-from internal.utils.logger import logger
 from config.config import get_config
-
 
 MONGODB_URI = get_config("mongodb_uri")
 DB_NAME = get_config("mongodb_database")
@@ -11,16 +7,9 @@ DB_NAME = get_config("mongodb_database")
 client = AsyncIOMotorClient(MONGODB_URI)
 database = client[DB_NAME]
 
-
-# Check the database connection at startup
-async def _check_connection():
-    await database.command("ping")
-
-try:
-    asyncio.run(_check_connection())
-    logger.info("Successfully connected to the database.")
-except Exception as e:
-    logger.error(f"Failed to connect to the database. Details: {e}")
-    sys.exit(1)
-
 users_collection = database["users"]
+books_collection = database["books"]
+
+
+async def check_connection():
+    await database.command("ping")
---
+++ internal/models/book.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from bson import ObjectId
+from datetime import datetime
+
+
+class Book(BaseModel):
+    id: str = Field(default_factory=lambda: str(ObjectId()), alias="_id")
+    title: str
+    authors: List[str]
+    categories: List[str]
+    language: Optional[str] = None
+    page_count: Optional[int] = None
+    isbn: Optional[str] = None
+    publisher: Optional[str] = None
+    cover_image: Optional[str] = None
+    description: Optional[str] = None
+    borrowed: bool = False
+    borrow_count: int = 0
+    return_date: Optional[datetime] = None
+    added_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+    available_copies: int = 1
+    total_copies: int = 1
+    last_borrowed_by: Optional[str] = None
+
+    def __setattr__(self, name, value):
+        if name == "id" and hasattr(self, "id"):
+            raise AttributeError(
+                "The 'id' field is immutable and cannot be changed after creation."
+            )
+        super().__setattr__(name, value)
