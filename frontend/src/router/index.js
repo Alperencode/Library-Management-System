98685f8cbd22dff2cbdb9794bbf97685da947944
emirhangokturk <emirhangokturk323@gmail.com>
2025-03-31 14:51:09 +0300

    feat: add user-page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,34 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/views/Auth/Login.vue";
+import Register from "@/views/Auth/Register.vue";
 import HomeView from "@/views/HomeView.vue";
 import MeetingDetails from "@/views/MeetingDetails.vue";
-import Register from "@/views/Auth/Register.vue";
+import UserPage from "@/views/UserPage.vue";
+
+// UserPage içindeki alt bileşenler
+import AccountManagement from "@/views/AccountManagement.vue";
+import BorrowedBooks from "@/views/BorrowedBooks.vue";
+import RequestedBooks from "@/views/RequestedBooks.vue";
+import NotifyMeList from "@/views/NotifyMe.vue";
+import BorrowHistory from "@/views/BorrowHistory.vue";
 
 const routes = [
-  { path: '/', component: HomeView }, 
+  { path: "/", component: HomeView },
   { path: "/login", component: Login },
   { path: "/register", component: Register },
   { path: "/meetings", component: MeetingDetails },
+
+  {
+    path: "/user-page",
+    component: UserPage, // Sidebarı sabitledim
+    children: [
+      { path: "account", component: AccountManagement },
+      { path: "borrowed", component: BorrowedBooks },
+      { path: "requested", component: RequestedBooks },
+      { path: "notify", component: NotifyMeList },
+      { path: "history", component: BorrowHistory },
+    ],
+  },
 ];
 
 const router = createRouter({
---
+++ frontend/src/views/BorrowHistory.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Borrow History</h1>
+    <p>Geçmişte ödünç aldığın kitaplar burada.</p>
+  </div>
+</template>
