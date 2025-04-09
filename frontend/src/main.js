4cacfac00c1e8935bfb9f1a32d0786396487a4ec
simalarifoglu <fatmasimal.arifoglu@gmail.com>
2025-04-09 20:00:22 +0300

    fix: book details page buttons edited

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store"; 
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap';
@@ -9,4 +12,5 @@ import 'bootstrap';
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.use(Toast)
 app.mount("#app");
