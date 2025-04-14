683a8905450ffce55302f0fdb7651253f626470f
Alperen Ağa <alperencode@gmail.com>
2025-04-15 02:40:26 +0300

    fix: restore read-write router and update JTW max-age

--- rfid-server/internal/tokens/tokens.py
+++ rfid-server/internal/tokens/tokens.py
@@ -1,16 +1,14 @@
-# utils/token.py
-
 from fastapi import Response
 from datetime import timedelta, datetime
-import jwt
 from config.config import get_config
+import jwt
 
 
 def create_scanned_book_token(book_isbn: str, response: Response):
     payload = {
         "book_isbn": book_isbn,
         "scanned": True,
-        "exp": datetime.now() + timedelta(minutes=5)
+        "exp": datetime.now() + timedelta(minutes=120)
     }
 
     token = jwt.encode(payload, get_config("secret_key"), algorithm=get_config("algorithm"))
@@ -20,7 +18,7 @@ def create_scanned_book_token(book_isbn: str, response: Response):
         key="scanned_book",
         value=token,
         httponly=True,
-        max_age=300,
+        max_age=60*120,
         **cookie_opts
     )
 
