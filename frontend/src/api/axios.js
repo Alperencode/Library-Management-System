edc8ebe38218c9fc7c5353ff50561e66e4e5b4c6
Şimal ARİFOĞLU <117273794+simalarifoglu@users.noreply.github.com>
2025-04-15 20:18:17 +0300

    feat: Show toast success messages (#62)

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import store from '@/store'
+import { createToastInterface } from 'vue-toastification'
 
 const protocol = window.location.protocol;
 const hostname = process.env.VUE_APP_API_HOST || window.location.hostname;
 const port = process.env.VUE_APP_API_PORT || 8000;
 const baseUrl = `${protocol}//${hostname}:${port}/api/v1`;
+const toast = createToastInterface()
 
 const api = axios.create({
   baseURL: baseUrl,
@@ -20,24 +22,24 @@ let isRefreshing = false
 let failedQueue = []
 let hasRedirected = false;
 
-const processQueue = (error) => {
-  failedQueue.forEach(({ resolve, reject }) => {
-    error ? reject(error) : resolve()
-  })
-  failedQueue = []
-}
-
 api.interceptors.response.use(
-  (res) => res,
+  (res) => {
+    if (res?.data?.success === false) {
+      const msg = res.data.message || res.data.detail
+      toast.error(msg)
+    }
+    return res
+  },
   async (err) => {
     const originalRequest = err.config
 
-    const ignoredPaths = ['/me','/refresh-token', '/login', '/register', '/books', '/books/', '/scan-book', '/'];
+    const ignoredPaths = ['/me', '/refresh-token', '/login', '/register', '/books', '/books/', '/scan-book', '/'];
     const ignoreCall = ignoredPaths.some(path => {
       if (path === '/') return originalRequest.url === '/';
       if (path.endsWith('/')) return originalRequest.url.startsWith(path);
       return originalRequest.url === path || originalRequest.url.startsWith(path + '/');
     });
+
     if (err.response?.status === 401 && !originalRequest._retry && !ignoreCall) {
       if (isRefreshing) {
         return new Promise((resolve, reject) => {
@@ -60,17 +62,29 @@ api.interceptors.response.use(
         hasRedirected = false;
         return api(originalRequest)
       } catch (error) {
-        processQueue(error)
+        const errorMessage =
+          error.response?.data?.message ||
+          error.response?.data?.detail
+
+        toast.error(errorMessage)
+
         const router = require('@/router').default;
         if (!hasRedirected && router.currentRoute.value.path !== '/login') {
           hasRedirected = true;
           router.push('/login');
         }
+
         return Promise.reject(error)
       } finally {
         isRefreshing = false
       }
     }
+
+    const generalMessage =
+      err.response?.data?.message ||
+      err.response?.data?.detail
+
+    toast.error(generalMessage)
 
     return Promise.reject(err)
   }
